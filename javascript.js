33e3fa393d37eacff007282e5fd9994988616bf5
peterjuniormassah-ux <peterjuniormassah@gmail.com>
2026-04-22 13:35:47 +0000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function scrollToContract(){
+    document.getElementByld("contract"). scrollnto-ViewTransition({
+        behaviour:"smooth"
+    });
+
+}
+
+function sendMessage(event){
+    event.preventDefault();
+    alert("Your order has been sent successfully");
+}
